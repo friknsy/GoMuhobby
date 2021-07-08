a8574065f5ffeb810d580d757590a86f799a20e3
wjdwns567 <57795527+wjdwns567@users.noreply.github.com>
2021-07-09 02:19:11 +0900

    21.07.09 02:19

---
+++ src/com/test/register/LoginDateDAO.java
@@ -0,0 +1,6 @@
+package com.test.register;
+
+public interface LoginDateDAO
+{
+	public int loginhit(LoginDateDTO u);
+}
---
+++ src/com/test/register/LoginDateDTO.java
@@ -0,0 +1,37 @@
+package com.test.register;
+
+public class LoginDateDTO
+{
+	private String login_num, login_date, uniq_id_num;
+
+	public String getLogin_num()
+	{
+		return login_num;
+	}
+
+	public void setLogin_num(String login_num)
+	{
+		this.login_num = login_num;
+	}
+
+	public String getLogin_date()
+	{
+		return login_date;
+	}
+
+	public void setLogin_date(String login_date)
+	{
+		this.login_date = login_date;
+	}
+
+	public String getUniq_id_num()
+	{
+		return uniq_id_num;
+	}
+
+	public void setUniq_id_num(String uniq_id_num)
+	{
+		this.uniq_id_num = uniq_id_num;
+	}
+	
+}
