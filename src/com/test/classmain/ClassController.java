e0069e81f4deba331720265903ff4611a561d898
hyyrim <realD1006@gmail.com>
2021-07-17 23:59:49 +0900

    ClassController 수정

--- src/com/test/classmain/ClassController.java
+++ src/com/test/classmain/ClassController.java
@@ -28,8 +28,79 @@ public class ClassController
 		System.out.println(dao.listNew());
 		
 		return "/WEB-INF/views/ClassMain.jsp";
+
+	}
+	
+	// 원데이클래스 전체
+	@RequestMapping(value = "/classall.action", method = RequestMethod.GET)
+	public String ClassAllList(Model model)
+	{
+		IClassDAO dao = sqlSession.getMapper(IClassDAO.class);
 		
-	}	
+		model.addAttribute("list", dao.listAll());
+		System.out.println(dao.listAll());
+		
+		return "/WEB-INF/views/ClassAll.jsp";
+		
+	}
+	
+	// 원데이클래스 추천 지역
+	@RequestMapping(value = "/classregion.action", method = RequestMethod.GET)
+	public String ClassRegionList(Model model, HttpServletRequest request)
+	{
+		String uniq_id_num = request.getParameter("uniq_id_num");
+		
+		IClassDAO dao = sqlSession.getMapper(IClassDAO.class);
+		
+		model.addAttribute("list", dao.listRegion(uniq_id_num));
+		//System.out.println(dao.listRegion(uniq_id_num));
+		
+		return "/WEB-INF/views/ClassRegion.jsp";
+		
+	}
+	
+	// 원데이클래스 추천 음악
+	@RequestMapping(value = "/classmusic.action", method = RequestMethod.GET)
+	public String ClassMusicList(Model model, HttpServletRequest request)
+	{
+		String uniq_id_num = request.getParameter("uniq_id_num");
+		
+		IClassDAO dao = sqlSession.getMapper(IClassDAO.class);
+		
+		model.addAttribute("list", dao.listMusic(uniq_id_num));
+		//System.out.println(dao.listMusic(uniq_id_num));
+		
+		return "/WEB-INF/views/ClassMusic.jsp";
+		
+	}
+	
+	// 클래스 개설 페이지 1
+	@RequestMapping(value = "/classopen1.action", method = RequestMethod.GET)
+	public String ClassOpen1(Model model)
+	{
+		IClassDAO dao = sqlSession.getMapper(IClassDAO.class);
+		
+		return "/WEB-INF/views/9ClassOpen1.jsp";
+	}
+	
+	// 클래스 개설 페이지 2
+	@RequestMapping(value = "/classopen2.action", method = RequestMethod.GET)
+	public String ClassOpen2(Model model)
+	{
+		IClassDAO dao = sqlSession.getMapper(IClassDAO.class);
+		
+		return "/WEB-INF/views/9ClassOpen2.jsp";
+	}
+	
+	// 클래스 개설 페이지 3
+	@RequestMapping(value = "/classopen3.action", method = RequestMethod.GET)
+	public String ClassOpen3(Model model)
+	{
+		IClassDAO dao = sqlSession.getMapper(IClassDAO.class);
+		
+		return "/WEB-INF/views/9ClassOpen3.jsp";
+	}
+
 
 	
 	// 원데이 클래스 상세
